c572a4230260f0051bee9504f2cb19ae43bdc1de
agbarrera <antoniogarci0309@gmail.com>
2025-03-03 18:12:10 +0100

    Correccion comentarios en las rutas

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,7 +3,7 @@ import { productController } from '../controllers/productController.js';
 
 const router = express.Router();
 
-// Rutas para obtener las todos los productos
+// Ruta para obtener todos los productos
 router.get('/productos', productController.getAllProducts)
 
 // Ruta para obtener productos por id
