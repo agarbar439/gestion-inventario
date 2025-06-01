a61741c5c2e5b62bc2cca04596bdbe8a82e124ca
agbarrera <antoniogarci0309@gmail.com>
2025-06-01 11:43:29 +0200

    Correcciones

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,46 +1,44 @@
 import jwt from 'jsonwebtoken';
-import config from '../config/config.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 // Middleware para verificar el token JWT y autenticacion
 export const authenticate = (req, res, next) => {
+    // Obtener el token de la cabecera Authorization
     const token = req.headers['authorization']?.split(' ')[1]; // Obtener solo el token
 
+    // Si no se encuentra el token, denegar el acceso
     if (!token) {
         return res.status(403).json({ error: 'Acceso denegado.' });
     }
 
+    // Verificacion del token
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        // Comprobar si el token a expirado o es invalido
         if (err) {
             return res.status(403).json({ error: 'Sesión inválida o expirada.' });
         }
 
+        // Si el token es valido, agregar la informacion del usuario a la solicitud
         req.user = user;
-        console.log("✅ Usuario autenticado:", user.nombre_usuario, "Rol:", user.rol);
 
+        // Llamar a la siguiente funcion del middleware
         next();
     });
 };
 
 // Middleware para verificar si el usuario es administrador
 export const isAdmin = (req, res, next) => {
-    console.log("Middleware isAdmin ejecutado");
-    console.log("Usuario en la solicitud:", req.user);
-
+    // Verificar si el usuario ha sido autenticDO
     if (!req.user) {
-        console.log("❌ Usuario no autenticado");
         return res.status(401).json({ message: "Usuario no autenticado." });
     }
 
-    console.log("Rol del usuario:", req.user.rol);
-
+    // Verificar si el usuario tiene rol de administrador
     if (req.user.rol !== "administrador") {
-        console.log("❌ Usuario sin permisos de administrador");
         return res.status(403).json({ message: "No tienes permisos para acceder a esta página." });
     }
 
-    console.log("✅ Usuario autorizado como administrador");
     next();
 };
 
